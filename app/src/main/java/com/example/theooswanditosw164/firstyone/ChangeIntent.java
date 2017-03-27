af3a6691115002aa1a84d441aac0346fa080d083
tosw164 <tosw164@aucklanduni.ac.nz>
2017-03-27 16:35:49 +1300

    Initial implementation of changeIntent (returns intent to be started)

--- app/src/main/java/com/example/theooswanditosw164/firstyone/ChangeIntent.java
+++ app/src/main/java/com/example/theooswanditosw164/firstyone/ChangeIntent.java
@@ -1,9 +1,23 @@
 package com.example.theooswanditosw164.firstyone;
 
+import android.app.Activity;
+import android.content.Intent;
+
 /**
  * Created by theooswanditosw164 on 26/03/17.
  */
 
 public class ChangeIntent {
-    
+
+    public Intent returnIntent(Activity current_activity, String layout_tag){
+        Class my_class = null;
+        try {
+            my_class = Class.forName("com.example.theooswanditosw164.firstyone." + layout_tag);
+
+        } catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+
+        return new Intent(current_activity, my_class);
+    }
 }
