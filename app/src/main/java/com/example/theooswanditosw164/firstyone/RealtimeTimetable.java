f71c35411eaa24302e8f949d0ddcf08f99441604
tosw164 <tosw164@aucklanduni.ac.nz>
2017-05-12 15:27:12 +1200

    Initialised .java and .xml for realtime timetable feature

---
+++ app/src/main/java/com/example/theooswanditosw164/firstyone/RealtimeTimetable.java
@@ -0,0 +1,13 @@
+package com.example.theooswanditosw164.firstyone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RealtimeTimetable extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_realtime_timetable);
+    }
+}
