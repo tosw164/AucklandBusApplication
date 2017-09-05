ca4f5c8db72c22e3a90fa79fef2ca0aaf343ac27
Theodore Oswandi <tosw164@aucklanduni.ac.nz>
2017-09-05 22:08:54 +1200

    Moved string containing API key into class with single static method
    + Modified the ATapiCall function to refer to the new class

---
+++ app/src/main/java/com/example/theooswanditosw164/firstyone/atapi/ATapiKey.java
@@ -0,0 +1,13 @@
+package com.example.theooswanditosw164.firstyone.atapi;
+
+/**
+ * Created by TheoOswandi on 5/09/2017.
+ */
+
+public class ATapiKey {
+    private static final String key = "92e47087b5c44366b1b74f96f42632df";
+
+    public static String getKey(){
+        return key;
+    }
+}
