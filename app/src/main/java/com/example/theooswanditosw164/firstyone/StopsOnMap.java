9dcb1cb37fca12e2524c1eb60a8c01583584e385
Theodore Oswandi <tosw164@aucklanduni.ac.nz>
2017-09-08 11:14:19 +1200

    Created floating action menu (#3)
    Implemented floating action menu with single button trigger
    
    - Updated dimens.xml to accommodate new components
    - Updated XML of stopsonmap with relevant components
    - Added container with FAB for menu items
    - Created animation and component hiding of menu items
    - Dumped a lot of 24dp PNGs for various icons
    
    Commit messages below:
    * Updated XML and dimensions of floating action buttons
    * Refactor and added basic animation for Floating Action Button menu
    * Added 24dp versions of images used
    * Updated XML and dimensions for floating action menu with labels
    * Implemented floating action menu with animation
        - Added third option for adding stop
        - Animated opening and closing
        - Handled hiding of labels and icons of menu items
        - Animated main button with rotation
        - Put initial component creation into method out of onCreate()
        - Modified buttons to use containers as now it contains a label
    * Removed unused commented code from StopsOnMap.java
    * Added some comments to StopsOnMap.java

--- app/src/main/java/com/example/theooswanditosw164/firstyone/StopsOnMap.java
+++ app/src/main/java/com/example/theooswanditosw164/firstyone/StopsOnMap.java
@@ -14,6 +14,7 @@ import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.example.theooswanditosw164.firstyone.atapi.AtApiRequests;
 import com.example.theooswanditosw164.firstyone.miscmessages.ToastMessage;
@@ -42,10 +43,13 @@ public class StopsOnMap extends FragmentActivity implements OnMapReadyCallback, 
     private final int MY_PERMISSION_ACCESS_LOCATION = 99;
 
     Button button1, button2;
-    FloatingActionButton fab1, fab2, fab3;
+    LinearLayout fab_container1, fab_container2, fab_container3;
+    FloatingActionButton main_fab, menu_fab1, menu_fab2, menufab3;
 
     ArrayList<Marker> list_of_markers;
     HashMap<String, Marker> map_of_markers_by_id;
+
+    private boolean fab_menu_open;
 
     private static final String TAG = StopsOnMap.class.getSimpleName();
 
@@ -54,22 +58,7 @@ public class StopsOnMap extends FragmentActivity implements OnMapReadyCallback, 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stops_on_map);
 
-        button1 = (Button) findViewById(R.id.stopsonmap_button1);
-        button1.setOnClickListener(this);
-        button1.setText("but1");
-
-        button2 = (Button) findViewById(R.id.stopsonmap_button2);
-        button2.setOnClickListener(this);
-        button2.setText("but2");
-
-        fab1 = (FloatingActionButton) findViewById(R.id.stopsonmap_fab1);
-        fab1.setOnClickListener(this);
-
-        fab2 = (FloatingActionButton) findViewById(R.id.stopsonmap_fab2);
-        fab2.setOnClickListener(this);
-
-        fab3 = (FloatingActionButton) findViewById(R.id.stopsonmap_fab3);
-        fab3.setOnClickListener(this);
+        setupComponents();
 
         location_manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
@@ -89,19 +78,63 @@ public class StopsOnMap extends FragmentActivity implements OnMapReadyCallback, 
         SupportMapFragment map_fragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         map_fragment.getMapAsync(this);
+    }
 
+    /**
+     * Sets up components used in this activity
+     */
+    private void setupComponents(){
+        button1 = (Button) findViewById(R.id.stopsonmap_button1);
+        button1.setOnClickListener(this);
+        button1.setText("but1");
+
+        button2 = (Button) findViewById(R.id.stopsonmap_button2);
+        button2.setOnClickListener(this);
+        button2.setText("but2");
+
+        //Logic for fab menu
+//        https://stackoverflow.com/questions/30699302/android-design-support-library-fab-menu
+
+        //Initialise main button and menu buttons and register listeners for each
+        main_fab = (FloatingActionButton) findViewById(R.id.stopsonmap_mainFAB);
+        main_fab.setOnClickListener(this);
+
+        menu_fab1 = (FloatingActionButton) findViewById(R.id.stopsonmap_FABmenu1);
+        menu_fab1.setOnClickListener(this);
+
+        menu_fab2 = (FloatingActionButton) findViewById(R.id.stopsonmap_FABmenu2);
+        menu_fab2.setOnClickListener(this);
+
+        menufab3 = (FloatingActionButton) findViewById(R.id.stopsonmap_FABmenu3);
+        menufab3.setOnClickListener(this);
+
+        //Initialise FAB menu item containers and sets them to invisible/GONE
+        fab_container2 = (LinearLayout) findViewById(R.id.stopsonmap_FABContainer1);
+        fab_container2.setVisibility(View.GONE);
+
+        fab_container1 = (LinearLayout) findViewById(R.id.stopsonmap_FABContainer2);
+        fab_container1.setVisibility(View.GONE);
+
+        fab_container3 = (LinearLayout) findViewById(R.id.stopsonmap_FABContainer3);
+        fab_container3.setVisibility(View.GONE);
+
+        fab_menu_open = false;
 
     }
 
+    /**
+     * Checks if either GPS or Network enabled so can poll location
+     * @return
+     */
     private boolean isLocationOn() {
         return location_manager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
                 location_manager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
     }
 
-
     @Override
     public void onMapReady(GoogleMap googleMap) {
         google_map = googleMap;
+        google_map.getUiSettings().setMapToolbarEnabled(false);
 
         //Add button to go to current location
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -128,7 +161,6 @@ public class StopsOnMap extends FragmentActivity implements OnMapReadyCallback, 
             google_map.setMyLocationEnabled(true); //TODO make sure this is safe
 
             //https://stackoverflow.com/questions/14441653/how-can-i-let-google-maps-api-v2-go-directly-to-my-location
-//            Location my_location = location_manager.getLastKnownLocation(location_manager.getBestProvider(new Criteria(), true));
             LatLng my_latlng = new LatLng(my_location.getLatitude(), my_location.getLongitude());
             google_map.moveCamera(CameraUpdateFactory.newLatLngZoom(my_latlng, 15));
 
@@ -182,7 +214,6 @@ public class StopsOnMap extends FragmentActivity implements OnMapReadyCallback, 
                         marker_to_add = google_map.addMarker(new MarkerOptions().position(new LatLng(stop_lat, stop_lng)).title(stop_id).snippet(short_name));
                         list_of_markers.add(marker_to_add);
                         map_of_markers_by_id.put(stop_id, marker_to_add);
-//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney").snippet("This is a snippet"));
                     }
                 }
 
@@ -201,19 +232,71 @@ public class StopsOnMap extends FragmentActivity implements OnMapReadyCallback, 
             case R.id.stopsonmap_button2:
                 Log.i(TAG, "button2");
                 break;
-            case R.id.stopsonmap_fab1:
-                Log.i(TAG, "fab1");
+            case R.id.stopsonmap_FABmenu1:
+                Log.i(TAG, "menu_fab2");
                 break;
-            case R.id.stopsonmap_fab2:
-                Log.i(TAG, "fab2");
+            case R.id.stopsonmap_FABmenu2:
+                Log.i(TAG, "menu_fab1");
                 break;
-            case R.id.stopsonmap_fab3:
-                Log.i(TAG, "fab3");
+            case R.id.stopsonmap_FABmenu3:
+                Log.i(TAG, "menu fab 3");
+                break;
+            case R.id.stopsonmap_mainFAB:
+                Log.i(TAG, "MainFab");
+                if (!fab_menu_open){
+                    //open fab menu
+                    animateFloatingActionMenuOpen();
+                } else {
+                    //close fab menu
+                    animateFloatingActionMenuClose();
+                }
                 break;
             default:
                 Log.i(TAG, "default");
                 break;
         }
+    }
+
+    /**
+     * Animates opening of floating action menu
+     */
+    private void animateFloatingActionMenuOpen(){
+        fab_menu_open = true;   //set flag
+
+        //Sets all the containers to visible from previous GONE state
+        fab_container1.setVisibility(View.VISIBLE);
+        fab_container2.setVisibility(View.VISIBLE);
+        fab_container3.setVisibility(View.VISIBLE);
+
+        //Rotate main button to make + into a X
+        main_fab.animate().rotationBy(45);
+
+        //Initialise offset values
+        float base_translate = getResources().getDimension(R.dimen.fab_menu_translate_base);
+        float translate_unit = getResources().getDimension(R.dimen.fab_menu_translate_unit);
+
+        //Animate floating action menu buttons to respective positions based on constants above
+        fab_container1.animate().translationY(-1 * (base_translate + translate_unit));
+        fab_container2.animate().translationY(-1 * (base_translate + 2*translate_unit));
+        fab_container3.animate().translationY(-1 * (base_translate + 3* translate_unit));
+    }
+
+    /**
+     * Animate the closing of action menu
+     */
+    private void animateFloatingActionMenuClose(){
+        fab_menu_open = false;      //set flag
+        main_fab.animate().rotationBy(-45); //Make Icon + again from x
+
+        //Return containers to behind main action button
+        fab_container1.animate().translationY(0);
+        fab_container2.animate().translationY(0);
+        fab_container3.animate().translationY(0);
+
+        //Hide containers to prevent accidental press and label from showing.
+        fab_container1.setVisibility(View.GONE);
+        fab_container2.setVisibility(View.GONE);
+        fab_container3.setVisibility(View.GONE);
     }
 
 
