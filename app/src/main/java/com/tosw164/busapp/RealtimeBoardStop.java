f2f155c10b7bcfbd332e266a5c6a93c40ed4a0a9
Theodore Oswandi <tosw164@aucklanduni.ac.nz>
2017-11-01 23:49:21 +1300

    Pull to refresh feature on realtime board (#18)
    Allow pulling down on listview to refresh the current list of scheduled bus trips for currently selected stop.
    
    Also added comments to RealtimeBoardStop.java
    
    
    
    * Wrapped realtime board listview in refreshlayout
    
    * Changed ID of the refresh layout component
    
    * Changed displayed arrival to departure for busses.
    Also added basic pull to refresh, with no
    functionality
    
    * Commenting RealtimeBoardStop.java
    
    * Updated pull to refresh to repopulate table.
    
    Also made bottom row of listview display current time

--- app/src/main/java/com/tosw164/busapp/RealtimeBoardStop.java
+++ app/src/main/java/com/tosw164/busapp/RealtimeBoardStop.java
@@ -1,6 +1,7 @@
 package com.tosw164.busapp;
 
 import android.os.AsyncTask;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -23,6 +24,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class RealtimeBoardStop extends AppCompatActivity{
@@ -35,6 +37,8 @@ public class RealtimeBoardStop extends AppCompatActivity{
     String stop_number;
     List<String> listview_contents;
     ListView timetable_view;
+    SwipeRefreshLayout refresh_layout;
+
 
     @Override
     protected void onCreate(Bundle bundle) {
@@ -55,6 +59,14 @@ public class RealtimeBoardStop extends AppCompatActivity{
         IS_FAVOURITE = (db.getFavouriteStop(stop_number) != null);
         db.close();
 
+        refresh_layout = (SwipeRefreshLayout) findViewById(R.id.realtime_stopsboard_swipe_refresh_layout);
+        refresh_layout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                new getRealtimeTimetableData().execute(stop_number);
+            }
+        });
+
         listview_contents = new ArrayList<String>();
 
         timetable_view = (ListView) findViewById(R.id.realtime_stopsboard_listview);
@@ -66,9 +78,14 @@ public class RealtimeBoardStop extends AppCompatActivity{
         });
 
         new getRealtimeTimetableData().execute(stop_number);
+
     }
 
-
+    /**
+     * Method that instantiates the favourite heart icon depending on if current stop is favourited
+     * @param menu
+     * @return
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_realtimeboard_for_stop, menu);
@@ -89,6 +106,7 @@ public class RealtimeBoardStop extends AppCompatActivity{
         switch (item.getItemId()){
             case R.id.menu_action_favorite:
 
+                //Toggle status of current stop number to/from favourite and notfavourite
                 IS_FAVOURITE = !IS_FAVOURITE;
 
                 if (IS_FAVOURITE){
@@ -112,20 +130,30 @@ public class RealtimeBoardStop extends AppCompatActivity{
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * Method that adds current stop number associated with realtime board to favourites table
+     *  in the database.
+     */
     private void addStopToFavourites(){
         SqliteTransportDatabase db = new SqliteTransportDatabase(getBaseContext());
         db.createFavouriteStop(stop_number, "TEST");
 
+        //TODO removed as used for debugging
         ToastMessage.makeToast(getBaseContext(), "added current stop from db");
         db.printAllFavouriteStops();
 
         db.close();
     }
 
+    /**
+     * Method that removes current stop number associated with realtime board from favourites
+     * table in the database.
+     */
     private void removeStopFromFavourites(){
         SqliteTransportDatabase db = new SqliteTransportDatabase(getBaseContext());
         db.deleteFavouriteStop(new FavouriteStop(stop_number, ""));
 
+        //TODO removed as used for debugging
         ToastMessage.makeToast(getBaseContext(), "removed current stop from db");
         db.printAllFavouriteStops();
 
@@ -146,45 +174,64 @@ public class RealtimeBoardStop extends AppCompatActivity{
 
 
     private class getRealtimeTimetableData extends AsyncTask<String, Void, List<String>>{
-
         @Override
         protected List<String> doInBackground(String... params) {
             List<String> timetable_data = null;
 
+            //Calls the API to get JSON representing routes for stop for given hours from now.
             JSONObject json = AtApiRequests.getRealtimeTimetableFromStopNumber(getBaseContext(), params[0], HOURS_TO_GET + "");
             try{
                 if (json.getString("status").equals("OK")){
                     JSONObject responses = json.getJSONObject("response");
                     JSONArray movements = responses.getJSONArray("movements");
                     timetable_data = new ArrayList<String>();
+
+                    //Iterates through each route item and extract estimated and scheduled times.
                     for(int i = 0; i < movements.length(); i++){
-                        JSONObject move = movements.getJSONObject(i);
-                        timetable_data.add("" + move.get("route_short_name") +
-                                "\t" + move.get("destinationDisplay") +
-                                "\t" + formatTime(move.get("scheduledArrivalTime").toString()) +
-                                " " + formatTime(move.get("expectedArrivalTime").toString()));
+                        JSONObject trip = movements.getJSONObject(i);
+
+                        //TODO format this nicer
+                        timetable_data.add("" + trip.get("route_short_name") +
+                                "\t" + trip.get("destinationDisplay") +
+                                "\t" + formatTime(trip.get("scheduledDepartureTime").toString()) +
+                                " " + formatTime(trip.get("expectedDepartureTime").toString()));
                     }
-                    timetable_data.add(" " + movements.length());
+                    timetable_data.add(" " + movements.length() + " " + Calendar.getInstance().getTime());
 
 
                 }
             } catch (JSONException e) {
+                //Checks if the API call returns nothing. If does then return null to
+                //let user know that no routes were found.
+                if (e.getMessage().equals("No value for movements")){
+                    return null;
+                }
+
+                //Print stack trace for debugging and identifying when unexpected behaviour observed
                 e.printStackTrace();
             }
+
+            //Return timetable data to populate listview the user sees
             return timetable_data;
         }
 
+        /**
+         * Method that updates the listview on the GUI side of the application
+         * @param incoming_routes is list of strings containig the incoming routes
+         *                        and expected & scheduled departure times
+         */
         @Override
         protected void onPostExecute(List<String> incoming_routes) {
             if (incoming_routes == null){
                 ToastMessage.makeToast(getBaseContext(), "No available routes");
-                return;
+            } else {
+                ArrayAdapter<String> array_adapter = new ArrayAdapter<String>(RealtimeBoardStop.this, android.R.layout.simple_list_item_1, incoming_routes);
+                timetable_view.setAdapter(array_adapter);
             }
 
-            ArrayAdapter<String> array_adapter = new ArrayAdapter<String>(RealtimeBoardStop.this, android.R.layout.simple_list_item_1, incoming_routes);
-            timetable_view.setAdapter(array_adapter);
-
-//            super.onPostExecute(incoming_routes);
+            if(refresh_layout.isRefreshing()){
+                refresh_layout.setRefreshing(false);
+            }
         }
     }
 }
