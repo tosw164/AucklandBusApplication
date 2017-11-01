21a09dc82eda5865aa0d68bcbce51e64804dd067
tosw164 <tosw164@aucklanduni.ac.nz>
2017-11-01 23:15:56 +1300

    Changed displayed arrival to departure for busses.
    Also added basic pull to refresh, with no
    functionality

--- app/src/main/java/com/tosw164/busapp/RealtimeBoardStop.java
+++ app/src/main/java/com/tosw164/busapp/RealtimeBoardStop.java
@@ -1,6 +1,7 @@
 package com.tosw164.busapp;
 
 import android.os.AsyncTask;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -35,6 +36,7 @@ public class RealtimeBoardStop extends AppCompatActivity{
     String stop_number;
     List<String> listview_contents;
     ListView timetable_view;
+    SwipeRefreshLayout refresh_layout;
 
     @Override
     protected void onCreate(Bundle bundle) {
@@ -55,6 +57,15 @@ public class RealtimeBoardStop extends AppCompatActivity{
         IS_FAVOURITE = (db.getFavouriteStop(stop_number) != null);
         db.close();
 
+        refresh_layout = (SwipeRefreshLayout) findViewById(R.id.realtime_stopsboard_swipe_refresh_layout);
+        refresh_layout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                System.out.println("REFRESH");
+                refresh_layout.setRefreshing(false);
+            }
+        });
+
         listview_contents = new ArrayList<String>();
 
         timetable_view = (ListView) findViewById(R.id.realtime_stopsboard_listview);
@@ -66,7 +77,10 @@ public class RealtimeBoardStop extends AppCompatActivity{
         });
 
         new getRealtimeTimetableData().execute(stop_number);
+
     }
+
+
 
 
     @Override
@@ -161,14 +175,19 @@ public class RealtimeBoardStop extends AppCompatActivity{
                         JSONObject move = movements.getJSONObject(i);
                         timetable_data.add("" + move.get("route_short_name") +
                                 "\t" + move.get("destinationDisplay") +
-                                "\t" + formatTime(move.get("scheduledArrivalTime").toString()) +
-                                " " + formatTime(move.get("expectedArrivalTime").toString()));
+                                "\t" + formatTime(move.get("scheduledDepartureTime").toString()) +
+                                " " + formatTime(move.get("expectedDepartureTime").toString()));
                     }
                     timetable_data.add(" " + movements.length());
 
 
                 }
             } catch (JSONException e) {
+                //Checks if the API call returns nothing. If does then return null to
+                //let user know that no routes were found.
+                if (e.getMessage().equals("No value for movements")){
+                    return null;
+                }
                 e.printStackTrace();
             }
             return timetable_data;
